5afb437301b989cba2f6ea3285e258e5d170f854
Jesusprz24 <perez.jesus1039@gmial.com>
2023-01-19 18:52:52 -0700

    finished server.js page and added information to readme.md

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-const PORT = precess.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
 const app = express();
 const allNotes = require('../Take-ya-notes/Develop/db/db.json');
 
-app.use??(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
@@ -15,34 +14,35 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 function createNewNote(body, notesArray) {
     const newNote = body;
     if (!Array.isArray(notesArray))
         notesArray = [];
-
+    
     if (notesArray.length === 0)
         notesArray.push(0);
 
-        body.id = notesArray[0];
-        notesArray[0]++;
+    body.id = notesArray[0];
+    notesArray[0]++;
 
-        notesArray.push(newNote);
-        fs.writeFileSync(
-            path.join(__dirname, '../Take-ya-notes/Develop/db/db.json'),
-            JSON.stringify(notesArray, null, 2)
-        );
-        return newNote;
+    notesArray.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '../Take-ya-notes/Develop/db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+
+    return newNote;
 }
 
 app.post('/api/notes', (req, res) => {
